f049acbe87f4bb69ff124f7b37f7e801040b9254
vox <drewdebelo@gmail.com>
2023-01-01 23:38:37 +0000

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Tom Finch on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
