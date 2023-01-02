1495938920d71d6a2df41442500216526c4243b0
vox <drewdebelo@gmail.com>
2023-01-02 00:02:00 +0000

    init

--- Form/ContentView.swift
+++ Form/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var bithDate = Date()
+    @State private var shouldSendNewsletter = false
+    @State private var numberOfLikes = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("Personal Information")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $bithDate, displayedComponents: .date)
+                }
+                
+                Section(header: Text("Actions")) {
+                    Toggle("Send Newslletter", isOn: $shouldSendNewsletter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms of Service", destination: URL(string: "https://seanallen.co")!)
+                }
+            }
+            
+            .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        hideKeyboard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    
+                    Button("Save", action: saveUser)
+                }
+            }
         }
-        .padding()
+        .accentColor(.red)
+    }
+        func saveUser() {
+            print("User Saved")
+        
     }
 }
 
@@ -24,3 +59,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
